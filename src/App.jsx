6543c482f08fd4e9fc425400cf59db68cd82e0dc
RoufRain <roufrain91@gmail.com>
2025-01-21 01:40:14 +0600

    square boared created

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,28 @@
-export default function App() {
-  return <h1 className="text-3xl text-bold">hello cut cut pro</h1>;
+function Square() {
+  return (
+    <button className="bg-white border border-black m-1 h-12 w-12 p-1 text-lg font-semibold">
+      X
+    </button>
+  );
+}
+export default function Board() {
+  return (
+    <>
+      <div>
+        <Square></Square>
+        <Square></Square>
+        <Square></Square>
+      </div>
+      <div>
+        <Square></Square>
+        <Square></Square>
+        <Square></Square>
+      </div>
+      <div>
+        <Square></Square>
+        <Square></Square>
+        <Square></Square>
+      </div>
+    </>
+  );
 }
