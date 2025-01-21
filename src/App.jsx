59a962c287b218ce88af0e4e6f088ea389d4b00f
RoufRain <roufrain91@gmail.com>
2025-01-21 14:54:58 +0600

    use useState,click functionaly,interectivity

--- src/App.jsx
+++ src/App.jsx
@@ -1,24 +1,35 @@
+import { useState } from "react";
+
 function Square() {
+  const [value, setValue] = useState(null);
+
+  function handleClick() {
+    setValue("X");
+  }
+
   return (
-    <button className="bg-white border border-black m-1 h-12 w-12 p-1 text-lg font-semibold">
-      X
+    <button
+      onClick={handleClick}
+      className="bg-white border border-black m-1 h-12 w-12 p-1 text-lg font-semibold"
+    >
+      {value}
     </button>
   );
 }
 export default function Board() {
   return (
     <>
-      <div>
+      <div className="flex">
         <Square></Square>
         <Square></Square>
         <Square></Square>
       </div>
-      <div>
+      <div className="flex">
         <Square></Square>
         <Square></Square>
         <Square></Square>
       </div>
-      <div>
+      <div className="flex">
         <Square></Square>
         <Square></Square>
         <Square></Square>
