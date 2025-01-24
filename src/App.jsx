8f34c980e8ff3639a305eab99161d6632bd92011
RoufRain <roufrain91@gmail.com>
2025-01-24 20:16:21 +0600

    up to board and each squares complete. after clicked on square they show differenet value

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,9 @@
 import { useState } from "react";
 
-function Square() {
-  const [value, setValue] = useState(null);
-
-  function handleClick() {
-    setValue("X");
-  }
-
+function Square({ value, onSquareClick }) {
   return (
     <button
-      onClick={handleClick}
+      onClick={onSquareClick}
       className="bg-white border border-black m-1 h-12 w-12 p-1 text-lg font-semibold"
     >
       {value}
@@ -17,22 +11,64 @@ function Square() {
   );
 }
 export default function Board() {
+  const [squares, setSquares] = useState(Array(9).fill(null));
+
+  const [xIsNext, setXIsNext] = useState(true);
+
+  function handleClick(i) {
+    const nextSquares = squares.slice();
+    if (xIsNext) {
+      nextSquares[i] = "X";
+    } else {
+      nextSquares[i] = "O";
+    }
+    setSquares(nextSquares);
+    setXIsNext(!xIsNext);
+  }
+
   return (
     <>
       <div className="flex">
-        <Square></Square>
-        <Square></Square>
-        <Square></Square>
+        <Square
+          value={squares[0]}
+          onSquareClick={() => handleClick(0)}
+        ></Square>
+        <Square
+          value={squares[1]}
+          onSquareClick={() => handleClick(1)}
+        ></Square>
+        <Square
+          value={squares[2]}
+          onSquareClick={() => handleClick(2)}
+        ></Square>
       </div>
       <div className="flex">
-        <Square></Square>
-        <Square></Square>
-        <Square></Square>
+        <Square
+          value={squares[3]}
+          onSquareClick={() => handleClick(3)}
+        ></Square>
+        <Square
+          value={squares[4]}
+          onSquareClick={() => handleClick(4)}
+        ></Square>
+        <Square
+          value={squares[5]}
+          onSquareClick={() => handleClick(5)}
+        ></Square>
       </div>
       <div className="flex">
-        <Square></Square>
-        <Square></Square>
-        <Square></Square>
+        <Square
+          value={squares[6]}
+          onSquareClick={() => handleClick(6)}
+        ></Square>
+        <Square
+          value={squares[7]}
+          onSquareClick={() => handleClick(7)}
+        ></Square>
+        <Square
+          value={squares[8]}
+          onSquareClick={() => handleClick(8)}
+        ></Square>
       </div>
     </>
   );
